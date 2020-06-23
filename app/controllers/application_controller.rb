29691c533bbcc7f2071680327f32a41937df7789
German Cruz <cruzgerman216@yahoo.com>
2020-06-23 23:32:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     puts params
     u = User.find_by(:username => params[:username])
-    puts u
+    puts u.id
     redirect '/tweets'
   end
   post '/signup' do
