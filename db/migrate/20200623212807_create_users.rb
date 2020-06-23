13cc8d766f37827e9060692511a23f7ea0873055
German Cruz <cruzgerman216@yahoo.com>
2020-06-23 21:29:31 +0000

    Automatically backed up by Learn

--- db/migrate/20200623212807_create_users.rb
+++ db/migrate/20200623212807_create_users.rb
@@ -1,4 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
+    create_table :users do |t|
+     t.integer "username"
+     t.integer "email"
+     t.integer "password_digest"
+    end
+    create_table :tweets do |t|
+     t.integer "content"
+    end
   end
 end
